e8f1f48a5bf9078428374d0a382bd2eda2512fd0
Richard Foge <fogeZombie@gmail.com>
2019-08-03 15:56:57 -0700

    Added UI
    Added Main Title.
    Added Auction with tabbed interface for information and ship.

---
+++ Source/GAS/Public/GAS_WidgetBase.h
@@ -0,0 +1,16 @@
+// Copyright BroachForge 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GAS_WidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API UGAS_WidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+};
